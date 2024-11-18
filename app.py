a14a19d7e5494a756779f91903f14a5d1f327fff
chelahcynthia <cchelangat989@gmail.com>
2024-11-18 21:28:25 +0300

    Initialize database

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 from flask import Flask, jsonify, request, abort
+import sqlite3
+
+app = Flask(__name__)
+
+# Database setup fn
+def init_db():
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(""
+                   "
+        
+    )
